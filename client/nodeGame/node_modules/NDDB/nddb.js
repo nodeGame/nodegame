9cf330ace8644732081985ba2695134b2d827228
Stefano Balietti <sbalietti@ethz.ch>
2011-12-14 17:55:54 +0100

    Going ahead with implementing and testing NDDB

--- client/nodeGame/node_modules/NDDB/nddb.js
+++ client/nodeGame/node_modules/NDDB/nddb.js
@@ -35,31 +35,54 @@
 	 */
 	
 	function NDDB (options, db) {
-	  this.options = options;
-	  this.log = options.log || console.log;
+		
+		// Default settings
+		this.options = null;
+		this.log = console.log;
+		
+		if (options) {
+			this.options = options;
+			this.log = options.log || this.log;
+		}
 	  
-	  this.DB = db || [];
-	  this.D = {};
+		this.D = {};		// The n-dimensional container for comparator functions
+		this.db = db || [];	// The actual database
+  		
 	  
 
-	  this.size = function() { return this.db.length };
+  		this.size = function() { return this.db.length };
 	};
 	
-	NDDB._comparator = function (o1, o2) {
-		if (!o1 && !o2) return 0;
-		if (!o1) return -1;
-		if (!o2) return 1;
-		if (o1 > o2) return 1;
-		if (o2 > o1) return -1;
-		return 0;
-	};
-	
+	NDDB.prototype.toString = function () {
+		var out = '';
+		for (var i=0; i< this.db.length; i++) {
+			out += this.db[i] + '\n'
+		}	
+		return out;
+	};	
+		
 	NDDB.prototype.set = function (d, comparator) {
-		this.D[d] = comparator || NDDB._comparator;
+		if (!d || !comparator) {
+			this.log('Cannot set empty property or empty comparator');
+			return false;
+		}
+		this.D[d] = comparator;
+		return true;
 	};
 
 	NDDB.prototype.comparator = function (d) {
-		return ('undefined' !== this.D[d]) ? this.D[d] : NDDB._comparator;
+		return ('undefined' !== typeof this.D[d]) ? this.D[d] : function (o1, o2) {
+//			console.log(o1);
+//			console.log(o2);
+			if (!o1 && !o2) return 0;
+			if (!o1) return -1;
+			if (!o2) return 1;
+			if (!o1[d]) return -1;
+			if (!o2[d]) return 1;
+			if (o1[d] > o2[d]) return 1;
+			if (o2[d] > o1[d]) return -1;
+			return 0;
+		};	
 	};
 	
 	NDDB.prototype.insert = function (o) {
@@ -115,8 +138,13 @@
 				return false;
 			}
 			
-			if (op === '=') op = '==';
-			
+			if (op === '=') {
+				op = '==';
+			}
+			// Encapsulating the value;
+			o = {};
+			o[d] = value;
+			value = o;
 		}
 		else if ('undefined' !== typeof value) {
 			raiseError(d,op,value);
@@ -135,7 +163,15 @@
 		var valid = this._analyzeQuery(d, op, value);		
 		if (!valid) return false;
 		
+		var d = valid.d;
+		var op = valid.op;
+		var value = valid.value;
+
 		var comparator = this.comparator(d);
+		
+		//this.log(comparator.toString());
+		//this.log(value);
+		
 		
 		var func = function (elem) {
 			try {	
@@ -200,6 +236,35 @@
 		}
 		
 		return new NDDB(this.options, out);
+	};
+	
+	NDDB.prototype.fetch = function (key, array) {
+		
+		switch (key) {
+			case 'VALUES':
+				var func = (array) ? GameBit.prototype.getValuesArray :
+									 GameBit.prototype.getValues ;
+				break;
+			case 'KEY_VALUES':
+				var func = (array) ? GameBit.prototype.getKeyValuesArray : 
+									 GameBit.prototype.getKeyValues; 
+				break;
+			default:
+				if (!array) return this.db;
+				var func = GameBit.prototype.toArray;
+		}
+		
+		var out = [];	
+		for (var i=0; i < this.db.length; i++) {
+			out.push(func.call(this.db[i]));
+		}	
+		
+		//console.log(out);
+		return out;
+	};
+	
+	NDDB.prototype.fetchArray = function (key) {
+		return this.fetch(key,true);
 	};
 	
 	
--- tests/NDDB_Test.js
+++ tests/NDDB_Test.js
@@ -1,25 +1,241 @@
 var Utils = require('../client/nodeGame/Utils.js');
 module.exports.Utils = Utils.Utils;
 
-console.log(Utils);
+var GameState = require('../client/nodeGame/GameState.js');
+module.exports.GameState = GameState.GameState;
+var GameState = GameState.GameState;
 
-//var GameState = require('../client/nodeGame/GameState.js');
-//module.exports.GameState = GameState.GameState;
-//var GameState = GameState.GameState;
+var GameBit = require('../client/nodeGame/GameStorage.js').GameBit;
 
 var NDDB = require('../client/nodeGame/node_modules/NDDB/nddb.js').NDDB;
-console.log(NDDB);
 
 var nddb = new NDDB();
+nddb.set('state', GameBit.compareState);
 
 var clients = ['a','b'];//['a','b','c','d'];
 var states = [1,2]; //[1,2,3,4];
 var ids = ['z','x'];//['z','x','c','v'];
 
+
 for (var i=0;i<clients.length;i++) {
 	for (var j=0;j<states.length;j++) {
 		for (var x=0;x<ids.length;x++) {
-			gs.add(clients[i], ids[x], Math.random(0,1), new GameState({state:states[j]}));
+			var gb = new GameBit({player: clients[i],
+								  key: ids[x],
+								  value: Math.random(0,1),
+								  state: new GameState({state:states[j]})
+			});
+			
+			nddb.insert(gb);
 		}
 	}
 }
+
+//console.log(nddb.toString());
+//
+//console.log('Default sort');
+//var out = nddb.sort();
+//console.log(out.toString());
+//
+//
+//console.log('Reverse');
+//out = nddb.reverse();
+//console.log(out.toString());
+//
+//console.log('Sort by Player');
+//nddb.sort('player');
+//console.log(out.toString());
+//
+//console.log('Sort by State');
+//out = nddb.sort('state');
+//console.log(out.toString());
+//
+//console.log('Sort by Key');
+//out = nddb.sort('key');
+//console.log(out.toString());
+
+//console.log('Get by value');
+//out = nddb.sort('value');
+//console.log(out.toString());
+
+//console.log('Select Key');
+//out = nddb.select('key','=','x');
+//console.log(out.toString());
+//
+//console.log('Select !Key');
+//out = nddb.select('key','!=','x');
+//console.log(out.toString());
+//
+//console.log('Select State');
+//out = nddb.select('state','>', new GameState({state: 1}));
+//console.log(out.toString());
+
+console.log('Join State');
+out = nddb.concat('state','state', 'joined');
+console.log(out);
+
+
+//var nddb = new NDDB();
+//
+//var clients = ['a','b'];//['a','b','c','d'];
+//var states = [1,2]; //[1,2,3,4];
+//var ids = ['z','x'];//['z','x','c','v'];
+//for (var i=0;i<clients.length;i++) {
+//	for (var j=0;j<states.length;j++) {
+//		for (var x=0;x<ids.length;x++) {
+//			var objs = [{mario: 'yes', paolo: 'no', r: Math.random()}]
+//			for (var o=0;o<objs.length; o++) {
+//				var gb = new GameBit({clients[i], ids[x], objs[o], new GameState({state:states[j]});
+//				nddb.insert(gb));
+//			}
+//		}
+//	}
+//}
+//
+//console.log(nddb.fetch());
+
+//console.log('Default sort (by Player)');
+//nddb.sort();
+//console.log(nddb.toString());
+////console.log(nddb);
+//
+//console.log('Reverse');
+//nddb.reverse();
+//console.log(nddb.toString());
+
+//console.log('Sort by Player');
+//nddb.sort();
+//console.log(nddb.toString());
+////console.log(nddb);
+//
+//console.log('Sort by State');
+//nddb.sortByState();
+//console.log(nddb.toString());
+//
+//console.log('Sort by Key');
+//nddb.sortByKey();
+//console.log(nddb.toString());
+
+//console.log('Get by Player');
+//var g = nddb.getByPlayer('b');
+//console.log(g);
+//
+//console.log('Get by State');
+//var g = nddb.getByState(new GameState({state:2}));
+//console.log(g);
+//
+//console.log('Get by Key');
+//var g = nddb.getByKey('z');
+//console.log(g);
+
+//console.log('Get Values');
+//var v = nddb.getValues();
+//console.log(v);
+
+//console.log('Get Values (reversed)');
+//nddb.reverse();
+//var v = nddb.getValues();
+//console.log(v);
+//
+//console.log('Get Value of Player \'a\'');
+//var v = nddb.getValues({player: 'a'});
+//console.log(v);
+
+//console.log('Get Key-Values');
+//var v = nddb.getKeyValues();
+//console.log(v);
+
+//console.log('Sort by Key-Values');
+//nddb.sortByValue('r');
+//
+//for (var gb in nddb.storage) {
+//	if (nddb.storage.hasOwnProperty(gb)) {
+//		console.log(nddb.storage[gb].value);
+//	}
+//}
+
+//Condition Filter
+//console.log('Array Test');
+
+//var obj = {a: 1, b: 2, c: 3, d: {a: 1, b: 2}};
+//
+//console.log(Utils);
+//
+//var ar = Utils.obj2Array(obj);
+//console.log(ar);
+//
+//var ar = Utils.obj2KeyedArray(obj);
+//console.log(ar);
+
+
+//var set = nddb.split();
+//console.log(set);
+//console.log(nddb.size());
+
+//set = set.select("value.r > 0.7");
+//console.log(set);
+//console.log(set.size());
+//
+//normalF = set.fetch();
+//kaF = set.fetchArray();
+//
+//console.log('Fetch Normal');
+//console.log(normalF);
+//console.log('Fetch Array');
+//console.log(kaF);
+
+//console.log('FetchArray');
+//var out = set.fetchArray();
+//console.log(out);
+
+//console.log('FetchValues');
+//var out = set.fetchValuesArray();
+//console.log(out);
+//
+//console.log('FetchKeyValues');
+//var out = set.fetchKeyValuesArray();
+//console.log(out);
+
+//console.log('Get');
+//var out = set.get();
+//console.log(out);
+
+
+//console.log('GameBit Test');
+//
+//var gb = new GameBit({state: 'a', player: '1', key: 'mamma', value: {a: 'yes', b: 'no'}});
+//console.log(gb);
+//
+//var out = gb.split();
+//console.log(out)
+//
+//console.log('toArray')
+//var out = gb.toArray();
+//console.log(out);
+
+//console.log('Values')
+//var out = gb.getValues();
+//console.log(out);
+//
+//console.log('Values Array');
+//var out = gb.getValuesArray();
+//console.log(out);
+//
+//console.log('KeyValues');
+//var out = gb.getKeyValues();
+//console.log(out);
+//
+//console.log('KeyValues Array');
+//var out = gb.getKeyValuesArray();
+//console.log(out);
+
+//console.log('Join Test');
+//
+//nddb = nddb.select('value.r > 0.5');
+//
+//var out = nddb.join('x','z','ahah');
+//console.log(out);
+//
+//console.log('1111111111111111111111');
+//var out = out.fetchArray();
+//console.log(out);
