87899073d4bebb4baab5afc51bad513b42e7cab5
Benedek Vartok <vartokb@student.ethz.ch>
2013-05-31 17:22:11 +0200

    Code cleanup

--- games/mygame/server/includes/game.client.js
+++ games/mygame/server/includes/game.client.js
@@ -11,52 +11,52 @@ var game = {};
 module.exports = game;
 
 game.globals = {
-	BIDDER: 1,
-	RESPONDENT: 0,
-	randomAccept: function(offer, other) {
-		var accepted = Math.round(Math.random());
-		console.log('randomaccept');
-		console.log(offer + ' ' + other);
-		if (accepted) {
-			node.emit('RESPONSE_DONE', 'ACCEPT', offer, other);
-			W.write(' You accepted the offer.');
-		}
-		else {
-			node.emit('RESPONSE_DONE', 'REJECT', offer, other);
-			W.write(' You rejected the offer.');
-		}
-	},
-	
-	isValidBid: function (n) {
-		if (!n) return false;
-		console.log(n)
-		console.log('valid?')
-		n = parseInt(n);
-		return !isNaN(n) && isFinite(n) && n >= 0 && n <= 100;
-	}
+    BIDDER: 1,
+    RESPONDENT: 0,
+    randomAccept: function(offer, other) {
+        var accepted = Math.round(Math.random());
+        console.log('randomaccept');
+        console.log(offer + ' ' + other);
+        if (accepted) {
+            node.emit('RESPONSE_DONE', 'ACCEPT', offer, other);
+            W.write(' You accepted the offer.');
+        }
+        else {
+            node.emit('RESPONSE_DONE', 'REJECT', offer, other);
+            W.write(' You rejected the offer.');
+        }
+    },
+
+    isValidBid: function(n) {
+        if (!n) return false;
+        console.log(n);
+        console.log('valid?');
+        n = parseInt(n, 10);
+        return !isNaN(n) && isFinite(n) && n >= 0 && n <= 100;
+    }
 };
 
 game.init = function() {
     console.log('INIT PLAYER!');
     W.setup('PLAYER');
-    
+
     var that = this;
     node.on('BID_DONE', function(offer, to) {
-	node.game.timer.stop();
-	W.getElementById('submitOffer').disabled = 'disabled';
-	node.set('offer', offer);
-	node.say(offer, 'OFFER', to);
-	W.write(' Your offer: ' +  offer + '. Waiting for the respondent... ');
+        node.game.timer.stop();
+        W.getElementById('submitOffer').disabled = 'disabled';
+        node.set('offer', offer);
+        node.say(offer, 'OFFER', to);
+        W.write(' Your offer: ' +  offer + '. Waiting for the respondent... ');
     });
-    
+
     node.on('RESPONSE_DONE', function(response, offer, from) {
-	node.set('response', {
-	    response: response,
-	    value: offer,
-	    from: from
-	});
-	node.say(response, response, from);
-	node.DONE();
+        node.set('response', {
+            response: response,
+            value: offer,
+            from: from
+        });
+        node.say(response, response, from);
+        node.DONE();
     });
 };
 
@@ -64,233 +64,233 @@ var gameover = function() {};
 
 
 var pregame = function() {
-	W.loadFrame('/mygame/html/prevar html', function(){
-		node.DONE();
-	});
-	console.log('Pregame');
+    W.loadFrame('/mygame/html/prevar html', function() {
+        node.DONE();
+    });
+    console.log('Pregame');
 };
 
 
-var instructions = function() {	
-	var that = this;
-	
-	//////////////////////////////////////////////
-	// nodeGame hint:
-	//
-	// The W object takes care of all 
-	// visual operation of the game. E.g.,
-	//
-	// W.loadFrame()
-	//
-	// loads an HTML file into the game screen, 
-	// and the execute the callback function 
-	// passed as second parameter.
-	//
-	/////////////////////////////////////////////
-	W.loadFrame('/mygame/html/instructions.html', function() {
-		var b = W.getElementById('read');
-		b.onclick = function() {
-			node.DONE();
-		};
-		
-		
-		////////////////////////////////////////////////
-		// nodeGame hint:
-		//
-		// node.env executes a function conditionally to 
-		// the environments defined in the configuration 
-		// options. 
-		//
-		// If the 'auto' environment was set to TRUE,
-		// then the function will be executed
-		//
-		////////////////////////////////////////////////
-		node.env('auto', function() {
-			
-			//////////////////////////////////////////////
-			// nodeGame hint:
-			//
-			// Emit an event randomly in a time interval 
-			// from 0 to 2000 milliseconds
-			//
-			//////////////////////////////////////////////
-			node.random.emit('DONE',2000);
-		});
-		
-		
-	});
-	console.log('Instructions');
+var instructions = function() {
+    var that = this;
+
+    //////////////////////////////////////////////
+    // nodeGame hint:
+    //
+    // The W object takes care of all
+    // visual operation of the game. E.g.,
+    //
+    // W.loadFrame()
+    //
+    // loads an HTML file into the game screen,
+    // and the execute the callback function
+    // passed as second parameter.
+    //
+    /////////////////////////////////////////////
+    W.loadFrame('/mygame/html/instructions.html', function() {
+        var b = W.getElementById('read');
+        b.onclick = function() {
+            node.DONE();
+        };
+
+
+        ////////////////////////////////////////////////
+        // nodeGame hint:
+        //
+        // node.env executes a function conditionally to
+        // the environments defined in the configuration
+        // options.
+        //
+        // If the 'auto' environment was set to TRUE,
+        // then the function will be executed
+        //
+        ////////////////////////////////////////////////
+        node.env('auto', function() {
+
+            //////////////////////////////////////////////
+            // nodeGame hint:
+            //
+            // Emit an event randomly in a time interval
+            // from 0 to 2000 milliseconds
+            //
+            //////////////////////////////////////////////
+            node.random.emit('DONE',2000);
+        });
+
+
+    });
+    console.log('Instructions');
 };
 
 var quiz = function() {
-    W.loadFrame('/mygame/html/quiz.html', function () {
-	console.log('QUIZ');
-	var b = W.getElementById('quiz');
-	b.onclick = function() {
-	    node.DONE();
-	};
+    W.loadFrame('/mygame/html/quiz.html', function() {
+        console.log('QUIZ');
+        var b = W.getElementById('quiz');
+        b.onclick = function() {
+            node.DONE();
+        };
     });
 };
-	
-var bidder = function () {
 
-	node.set('ROLE', 'BIDDER');
-	W.loadFrame('/mygame/html/bidder.html', function () {
-		
-		// Start the timer after an offer was received.
-		var options = {
-				milliseconds: 30000,
-				timeup: function(){
-					node.emit('BID_DONE', Math.floor(1+Math.random()*100), other);
-				}
-		};
-		
-		node.game.timer.restart(options);
+var bidder = function() {
 
-		var b = W.getElementById('submitOffer');
-		
-		node.env('auto', function() {
-			
-			//////////////////////////////////////////////
-			// nodeGame hint:
-			//
-			// Execute a function randomly
-			// in a time interval between 0 and 1 second
-			//
-			//////////////////////////////////////////////
-			node.random.exec(function() {
-				node.emit('BID_DONE', Math.floor(1+Math.random()*100), other);
-			}, 4000);
-		});
-		
-		b.onclick = function() {
-			var offer = W.getElementById('offer');
-			if (!that.isValidBid(offer.value)) {
-				W.writeln('Please enter a number between 0 and 100');
-				return;
-			}
-			node.emit('BID_DONE', offer.value, other);
-		};
-		
-		node.onDATA('ACCEPT', function (msg) {
-			W.write(' Your offer was accepted.');
-			node.random.emit('DONE', 3000);
-		});
-			
-		node.onDATA('REJECT', function (msg) {
-			W.write(' Your offer was rejected.');
-			node.random.emit('DONE', 3000);
-		});
-	});
+    node.set('ROLE', 'BIDDER');
+    W.loadFrame('/mygame/html/bidder.html', function() {
 
-};		
+        // Start the timer after an offer was received.
+        var options = {
+            milliseconds: 30000,
+            timeup: function() {
+                node.emit('BID_DONE', Math.floor(1+Math.random()*100), other);
+            }
+        };
+
+        node.game.timer.restart(options);
+
+        var b = W.getElementById('submitOffer');
+
+        node.env('auto', function() {
+
+            //////////////////////////////////////////////
+            // nodeGame hint:
+            //
+            // Execute a function randomly
+            // in a time interval between 0 and 1 second
+            //
+            //////////////////////////////////////////////
+            node.random.exec(function() {
+                node.emit('BID_DONE', Math.floor(1+Math.random()*100), other);
+            }, 4000);
+        });
+
+        b.onclick = function() {
+            var offer = W.getElementById('offer');
+            if (!that.isValidBid(offer.value)) {
+                W.writeln('Please enter a number between 0 and 100');
+                return;
+            }
+            node.emit('BID_DONE', offer.value, other);
+        };
+
+        node.onDATA('ACCEPT', function(msg) {
+            W.write(' Your offer was accepted.');
+            node.random.emit('DONE', 3000);
+        });
+
+        node.onDATA('REJECT', function(msg) {
+            W.write(' Your offer was rejected.');
+            node.random.emit('DONE', 3000);
+        });
+    });
+
+};
 
 var respondent = function() {
-	var other = msg.data.other;
-	node.set('ROLE', 'RESPONDENT');
-	
-	var options = {
-			milliseconds: 30000,
-			timeup: function() {
-				that.randomAccept(msg.data, other);
-			}
-	};
-	node.game.timer.init(options);
-	// set to 30:00
-	node.game.timer.updateDisplay();
-	
-	W.loadFrame('/mygame/html/resp.html', function () {
-		
-		node.onDATA('OFFER', function (msg) {
-								
-			// Start the timer after an offer was received.
-			node.game.timer.start();
-			
-			var offered = W.getElementById('offered');
-			W.write('You received an offer of ' + msg.data, offered);
-			offered.style.display = '';
-		
-			var accept = W.getElementById('accept');
-			var reject = W.getElementById('reject');
-		
-			node.env('auto', function() {
-				node.random.exec(function() {
-					that.randomAccept(msg.data, other);
-				}, 3000);
-			});
-			
-			accept.onclick = function() {
-				node.emit('RESPONSE_DONE', 'ACCEPT', msg.data, other);
-			};
-			
-			reject.onclick = function() {
-				node.emit('RESPONSE_DONE', 'REJECT', msg.data, other);
-			};
-		});
-	});	
+    var other = msg.data.other;
+    node.set('ROLE', 'RESPONDENT');
 
-	console.log('Ultimatum');
+    var options = {
+        milliseconds: 30000,
+        timeup: function() {
+            that.randomAccept(msg.data, other);
+        }
+    };
+    node.game.timer.init(options);
+    // set to 30:00
+    node.game.timer.updateDisplay();
+
+    W.loadFrame('/mygame/html/resp.html', function() {
+
+        node.onDATA('OFFER', function(msg) {
+
+            // Start the timer after an offer was received.
+            node.game.timer.start();
+
+            var offered = W.getElementById('offered');
+            W.write('You received an offer of ' + msg.data, offered);
+            offered.style.display = '';
+
+            var accept = W.getElementById('accept');
+            var reject = W.getElementById('reject');
+
+            node.env('auto', function() {
+                node.random.exec(function() {
+                    that.randomAccept(msg.data, other);
+                }, 3000);
+            });
+
+            accept.onclick = function() {
+                node.emit('RESPONSE_DONE', 'ACCEPT', msg.data, other);
+            };
+
+            reject.onclick = function() {
+                node.emit('RESPONSE_DONE', 'REJECT', msg.data, other);
+            };
+        });
+    });
+
+    console.log('Ultimatum');
 };
 
 var solowait = function() {
-	
-	W.loadFrame('/mygame/html/solo.html', function () {
-		var options = {
-				milliseconds: 0,
-				timeup: function() {
-					that.randomAccept(msg.data, other);
-				}
-		};
-		node.game.timer.init(options);
-		// set to 30:00
-		node.game.timer.updateDisplay();
-		
-		//////////////////////////////////////////////
-		// nodeGame hint:
-		//
-		// node.DONE() communicates to the server that 
-		// the player has completed the current state.
-		//
-		// What happens next depends on the game. 
-		// In this game the player will have to wait
-		// until all the other players are also "done".
-		// 
-		/////////////////////////////////////////////
-		node.DONE();
-	});
 
-	console.log('solodone');
-		
+    W.loadFrame('/mygame/html/solo.html', function() {
+        var options = {
+            milliseconds: 0,
+            timeup: function() {
+                that.randomAccept(msg.data, other);
+            }
+        };
+        node.game.timer.init(options);
+        // set to 30:00
+        node.game.timer.updateDisplay();
+
+        //////////////////////////////////////////////
+        // nodeGame hint:
+        //
+        // node.DONE() communicates to the server that
+        // the player has completed the current state.
+        //
+        // What happens next depends on the game.
+        // In this game the player will have to wait
+        // until all the other players are also "done".
+        //
+        /////////////////////////////////////////////
+        node.DONE();
+    });
+
+    console.log('solodone');
+
 };
 
 var questionnaire = function() {
-	W.loadFrame('/mygame/html/postgame.html', function(){
-		node.env('auto', function(){
-			node.random.emit('DONE');
-		});
-	});
-	console.log('Postgame');
+    W.loadFrame('/mygame/html/postgame.html', function() {
+        node.env('auto', function() {
+            node.random.emit('DONE');
+        });
+    });
+    console.log('Postgame');
 };
 
 var endgame = function() {
-	W.loadFrame('/mygame/html/ended.html', function(){
-		node.on('WIN', function(msg) {
-			W.write('Your earning in the game is: ' + msg.data);
-		});
-	});
-	
-	console.log('Game ended');
+    W.loadFrame('/mygame/html/ended.html', function() {
+        node.on('WIN', function(msg) {
+            W.write('Your earning in the game is: ' + msg.data);
+        });
+    });
+
+    console.log('Game ended');
 };
 
 var gameplay = function() {
-    W.loadFrame('/mygame/html/game.html', function(){
-	node.env('auto', function(){
-	    node.random.emit('DONE');
-	});
-	var b = W.getElementById('play');
-	b.onclick = function() {
-	    node.DONE();
-	};
+    W.loadFrame('/mygame/html/game.html', function() {
+        node.env('auto', function() {
+            node.random.emit('DONE');
+        });
+        var b = W.getElementById('play');
+        b.onclick = function() {
+            node.DONE();
+        };
     });
     console.log('Game');
 };
--- games/mygame/server/includes/game.logic.js
+++ games/mygame/server/includes/game.logic.js
@@ -8,128 +8,127 @@ module.exports = logic;
 // but could be loaded from the database
 
 logic.tutorialStage = {
-	 	id: 'tutorial',
-		cb: function() {
+    id: 'tutorial',
+    cb: function() {
 
-            console.log('TUTORIAL STAGE');
-            return;
+        console.log('TUTORIAL STAGE');
+        return;
 
-			// everybody plays the same stage 
-			// as it was defined in game.client, 
-			// and already setup on the client
-			var playersMap = node.RoleMapper('ALL', 'tutorial');
-		
-			channel.startSubGame({
-				id: 'tutorial',
-				players: playersMap
-				// no logic process will be loaded, 
-				// clients will just exchange messages
-				// and their exchange registered
-				// No special variables are passed
-				// logics: null,
-				// env: null,
-			});
-			
-		},
-		onexit: function() {
-			
-			// We can decide on real time how the logic develops
-//				if (node.game.globals.A) {
-//					node.game.plot.next('gameA');
-//				}
-//				else {
-//					node.game.plot.next('gameB');
-//				}
-			
-		}
+        // everybody plays the same stage
+        // as it was defined in game.client,
+        // and already setup on the client
+        var playersMap = node.RoleMapper('ALL', 'tutorial');
+
+        channel.startSubGame({
+            id: 'tutorial',
+            players: playersMap
+            // no logic process will be loaded,
+            // clients will just exchange messages
+            // and their exchange registered
+            // No special variables are passed
+            // logics: null,
+            // env: null,
+        });
+
+    },
+    onexit: function() {
+
+        // We can decide on real time how the logic develops
+//              if (node.game.globals.A) {
+//                  node.game.plot.next('gameA');
+//              }
+//              else {
+//                  node.game.plot.next('gameB');
+//              }
+
+        }
 };
 
 logic.gameStage = {
-		id: 'game',
-		init: function() {
-            console.log('INITING THE GAME STAGE')
-			// something
-			return true;
-		},
-		cb: function() {
+    id: 'game',
+    init: function() {
+        console.log('INITING THE GAME STAGE');
+        // something
+        return true;
+    },
+    cb: function() {
+        console.log('GAME STAGE');
+        return;
 
-            console.log('GAME STAGE');
-            return;
+        // creates groups here
 
-			// creates groups here
-			
-			var groups = node.GroupManager(node.game.pl, {
-				size: 2,
-				remaining: 'ADD'
-			});
-			    
-			// Setting up each client
-			groups.each(function(group) {	
-				var CLIENT_B = group[0];
-				var CLIENT_A = group[1];
-			    
-				var rolesMap = node.RoleMapper({
-					BIDDER: {
-						player: CLIENT_B,
-						stage: 'bidder',
-						env: {
-							otherVar: 'foo1'
-						}
-					},
-					RESPONDENT: {
-						player: CLIENT_A,
-						stage: 'respondent',
-						env: {
-							otherVar: 'foo2'
-						}
-					}
-				});
-				
-				channel.startSubGame({
-					id: 'gameround',
-					players: rolesMap
-					// no logic process will be loaded, 
-					// clients will just exchange messages
-					// and their exchange registered
-					// No special variables are passed
-					// logics: null,
-					// env: null,
-				});
-				
-			});
-		}
+        var groups = node.GroupManager(node.game.pl, {
+            size: 2,
+            remaining: 'ADD'
+        });
+
+        // Setting up each client
+        groups.each(function(group) {
+            var CLIENT_B = group[0];
+            var CLIENT_A = group[1];
+
+            var rolesMap = node.RoleMapper({
+                BIDDER: {
+                    player: CLIENT_B,
+                    stage: 'bidder',
+                    env: {
+                        otherVar: 'foo1'
+                    }
+                },
+                RESPONDENT: {
+                    player: CLIENT_A,
+                    stage: 'respondent',
+                    env: {
+                        otherVar: 'foo2'
+                    }
+                }
+            });
+
+            channel.startSubGame({
+                id: 'gameround',
+                players: rolesMap
+                // no logic process will be loaded,
+                // clients will just exchange messages
+                // and their exchange registered
+                // No special variables are passed
+                // logics: null,
+                // env: null,
+            });
+
+        });
+    }
 };
 
 logic.questionnaireStage = {
-	 	id: 'questionnaire', 
-		cb: function() {
-			console.log('QUESTIONNAIRE STAGE');
-            return;
+    id: 'questionnaire',
+    cb: function() {
+        console.log('QUESTIONNAIRE STAGE');
+        return;
 
-			// everybody plays the same stage 
-			// as it was defined in game.client, 
-			// and already setup on the client
-			var playersMap = node.RoleMapper('ALL', 'questionnaire');
-			
-			channel.startSubGame({
-				id: 'questionnaire',
-				players: playersMap
-				// no logic process will be loaded, 
-				// clients will just exchange messages
-				// and their exchange registered
-				// No special variables are passed
-				// logics: null,
-				// env: null,
-			});
-			
-			
-			// Alternatively we could not spawn a subgame 
-			// and directly invoke a new stage on all the clients
-			// maybe it is confusing for the developer though
-			// node.remote.gamecommand('exec', 'questionnaire');
+        // everybody plays the same stage
+        // as it was defined in game.client,
+        // and already setup on the client
+        var playersMap = node.RoleMapper('ALL', 'questionnaire');
 
-			
-		}
+        channel.startSubGame({
+            id: 'questionnaire',
+            players: playersMap
+            // no logic process will be loaded,
+            // clients will just exchange messages
+            // and their exchange registered
+            // No special variables are passed
+            // logics: null,
+            // env: null,
+        });
+
+
+        // Alternatively we could not spawn a subgame
+        // and directly invoke a new stage on all the clients
+        // maybe it is confusing for the developer though
+        // node.remote.gamecommand('exec', 'questionnaire');
+
+
+    }
 };
 
 
