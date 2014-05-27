dc6b920e79c37ae5978b072f023ea73a088cf2cf
Stefano Balietti <futur.dorko@gmail.com>
2014-05-27 17:32:56 +0200

    Removed old games from index

--- games/ultimatum/server/conf/http.js
+++
@@ -1,8 +0,0 @@
-module.exports = configure;
-
-
-function configure (app) {
-	// Special configuration goes here
-	// See http://expressjs.com/guide.html
-	return true;
-}
--- games/ultiturk/server/conf/sio.js
+++
@@ -1,8 +0,0 @@
-module.exports = configure;
-
-function configure (sio) {
-    sio.enable('browser client etag');
-    sio.set('log level', -1);
-    
-    return true;
-}
