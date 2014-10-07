e142d80cf6bb06def8c1cc54b228ad249227b9f0
Stefano Balietti <futur.dorko@gmail.com>
2014-10-08 00:36:27 +0200

    updated windows install script

--- bin/install.cmd
+++ bin/install.cmd
@@ -7,6 +7,7 @@ setlocal enableextensions enabledelayedexpansion
 
 :: Clone the main repo.
 git clone https://github.com/nodeGame/nodegame.git
+
 cd nodegame
 
 :: Install the dependencies.
@@ -21,43 +22,37 @@ git clone https://github.com/nodeGame/shelf.js.git
 git clone https://github.com/nodeGame/descil-mturk.git
 git clone https://github.com/nodeGame/nodegame-db.git
 git clone https://github.com/nodeGame/nodegame-mongodb.git
-npm install smoosh
-npm install ya-csv
-npm install commander
-npm install docker
+call npm install smoosh
+call npm install ya-csv
+call npm install commander
+call npm install docker
 
-:: Add symbolic links to given dependencies that are in nodegame/node_modules
-:: (e.g. JSUS, NDDB)
-
-:link_deps
-mkdir node_modules
-cd node_modules
-for /l %%x in (1, 1, %argCount%) do (
-    mklink "../../%%x" "."
-)
-GOTO:EOF
+:: Temporary Fix
+call npm install resolve
+call npm install wrench
 
 :: Install sub-dependencies; link to tracked dependencies.
+
 cd JSUS
-npm install
+:: call npm install
 
 cd ../descil-mturk
-link_deps JSUS NDDB
-npm install
+:: call:linkDeps JSUS NDDB
+call npm install
 
 cd ../nodegame-mongodb
-npm install
+call npm install
 
 cd ../nodegame-client
-link_deps JSUS NDDB shelf.js
-npm install
+:: call:linkDeps JSUS NDDB shelf.js
+call npm install
 
 cd ../nodegame-server
-link_deps JSUS NDDB shelf.js nodegame-widgets
-npm install
+:: call:linkDeps JSUS NDDB shelf.js nodegame-widgets
+call npm install
 
-:: Patching express connect.
-robocopy bin/ng.connect.static.js.copy node_modules/express/node_modules/connect/lib/middleware/static.js
+:: Patching express connect (copy + rename).
+xcopy /Y bin\static.js node_modules\express\node_modules\connect\lib\middleware\static.js
 
 :: Rebuild js files.
 cd bin
@@ -65,15 +60,29 @@ node make build-client -a -o nodegame-full
 
 :: Install ultimatum game.
 cd ../../../
-git clone https://github.com/nodeGame/ultimatum games/ultimatum.git
 
+git clone https://github.com/nodeGame/ultimatum.git games/ultimatum
 :: Executes the following commands to try out the ultimatum game.
 
 :: Start the ultimatum game.
 :: node start/ultimatum-server
+
 
 :: Open two browser tabs for two players at the address:
 :: http://localhost:8080/ultimatum/
 :: Open the admin console at:
 :: http://localhost:8080/ultimatum/monitor.htm
 :: See the wiki documentation to modify settings.
+
+:: Not used for now.
+
+:: Add symbolic links to given dependencies that are in nodegame/node_modules
+:: (e.g. JSUS, NDDB)
+
+:: :linkDeps
+:: mkdir node_modules
+:: cd node_modules
+:: for /l %%x in (1, 1, %argCount%) do (
+::     mklink "../../%%x" "."
+:: )
+:: GOTO:EOF
