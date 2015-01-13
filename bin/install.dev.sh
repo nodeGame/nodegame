385797587c80eafcc0fd9886451c661e6a3ddc8d
Benedek Vartok <vartokb@student.ethz.ch>
2015-01-13 11:29:43 +0100

    Restructured dev install script, handles git-hooks

--- bin/install.dev.sh
+++ bin/install.dev.sh
@@ -1,29 +1,8 @@
 #!/bin/bash
-# nodeGame install from sources script
+# nodeGame install development version from sources script
 # Copyright(c) 2015 Stefano Balietti
 # MIT Licensed
 
-# Clone the repo.
-git clone git@github.com:nodeGame/nodegame.git
-cd nodegame
-
-# Install the dependencies.
-mkdir -p node_modules
-cd node_modules
-git clone git@github.com:nodeGame/nodegame-client
-git clone git@github.com:nodeGame/nodegame-server
-git clone git@github.com:nodeGame/nodegame-window
-git clone git@github.com:nodeGame/nodegame-widgets
-git clone git@github.com:nodeGame/JSUS
-git clone git@github.com:nodeGame/NDDB
-git clone git@github.com:nodeGame/shelf.js
-git clone git@github.com:nodeGame/descil-mturk
-git clone git@github.com:nodeGame/nodegame-db
-git clone git@github.com:nodeGame/nodegame-mongodb
-npm install smoosh
-npm install ya-csv
-npm install commander
-npm install docker
 
 # Add symbolic links to given dependencies that are in nodegame/node_modules
 # (e.g. JSUS, NDDB).
@@ -37,36 +16,71 @@ function link_deps {
     )
 }
 
+
+# List of all sub-modules on GitHub to clone:
+gitmodules="nodegame-client nodegame-server nodegame-window nodegame-widgets "\
+"JSUS NDDB shelf.js descil-mturk nodegame-db nodegame-mongodb"
+
+
+# Clone the repo, copy Git hooks.
+git clone git@github.com:nodeGame/nodegame.git
+cd nodegame
+cp git-hooks/* .git/hooks/
+
+# Install the dependencies.
+mkdir -p node_modules
+cd node_modules
+for module in $gitmodules; do git clone "git@github.com:nodeGame/${module}.git"; done
+npm install smoosh
+npm install ya-csv
+npm install commander
+npm install docker
+
 # Install sub-dependencies, link to tracked dependencies.
-cd JSUS
-npm install
+(
+    cd JSUS
+    npm install
+)
 
-cd ../descil-mturk
-link_deps JSUS NDDB
-npm install
+(
+    cd descil-mturk
+    link_deps JSUS NDDB
+    npm install
+)
 
-cd ../nodegame-mongodb
-npm install
+(
+    cd nodegame-mongodb
+    npm install
+)
 
-cd ../nodegame-client
-link_deps JSUS NDDB shelf.js
-npm install
+(
+    cd nodegame-client
+    link_deps JSUS NDDB shelf.js
+    npm install
+)
 
-cd ../nodegame-server
-link_deps JSUS NDDB shelf.js nodegame-widgets
-npm install
+(
+    cd nodegame-server
+    link_deps JSUS NDDB shelf.js nodegame-widgets
+    npm install
 
-# Patching express connect.
-patch node_modules/express/node_modules/connect/lib/middleware/static.js < \
-  bin/ng.connect.static.js.patch
+    # Patch express connect.
+    patch node_modules/express/node_modules/connect/lib/middleware/static.js < \
+      bin/ng.connect.static.js.patch
 
-# Rebuild js files.
-cd bin
-node make build-client -a -o nodegame-full
+    # Rebuild js files.
+    node bin/make.js build-client -a -o nodegame-full
+)
 
-# Install ultimatum game.
-cd ../../..
+# Copy Git hooks.
+for module in $gitmodules
+do  cp ../git-hooks/* "${module}/.git/hooks/"
+done
+
+# Install ultimatum game under nodegame/games.
+cd ..
 git clone git@github.com:nodeGame/ultimatum games/ultimatum
+cp git-hooks/* games/ultimatum/.git/hooks/
 
 
 # Execute the following commands to try out the ultimatum game.
